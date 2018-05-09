7df0cebf712f0124a7f80635bb7428d780cc709c
codeBooks <my-books@qq.com>
2018-05-09 12:05:09 +0800

    append some code

--- src/bean/Source/InternetdataInfo.java
+++ src/bean/Source/InternetdataInfo.java
@@ -6,29 +6,35 @@ public class InternetdataInfo {
 	String date;
 	
 	private UserInfo userInfo;
-	
-	
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getUrl() {
 		return url;
 	}
+
 	public void setUrl(String url) {
 		this.url = url;
 	}
+
 	public String getDate() {
 		return date;
 	}
+
 	public void setDate(String date) {
 		this.date = date;
 	}
+
 	public UserInfo getUserInfo() {
 		return userInfo;
 	}
+
 	public void setUserInfo(UserInfo userInfo) {
 		this.userInfo = userInfo;
 	}
